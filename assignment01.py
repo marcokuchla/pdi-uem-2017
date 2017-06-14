f3f2428d374b7e2dd0d15f7dd4fbef9005b2d5df
Marco Braga <marcokuchla@gmail.com>
2017-06-14 16:28:50 -0300

    Fixed bug in get_linspace function. New algorithm using matrices product.

--- assignment01.py
+++ assignment01.py
@@ -1,17 +1,45 @@
 #!/usr/bin/env python3
-"""Trabalho 'I love Hue'"""
+"""Assignment 01: 'I love Hue' game's grid."""
 import cv2
 import numpy as np
 
 def main():
-    c1 = np.random.randint(0, 255, 3)
-    c2 = np.random.randint(0, 255, 3)
-    c3 = np.random.randint(0, 255, 3)
-    c4 = np.random.randint(0, 255, 3)
-    img = up_sampling(get_image(5, 5, c1, c2, c3, c4))
+    """Entry point."""
+    # tl = np.array([0, 0, 0])
+    # tr = np.array([85, 85, 85])
+    # br = np.array([170, 170, 170])
+    # bl = np.array([255, 255, 255])
+    tl = np.random.randint(0, 255, 3)
+    tr = np.random.randint(0, 255, 3)
+    bl = np.random.randint(0, 255, 3)
+    br = np.random.randint(0, 255, 3)
+    img = up_sampling(get_image(5, 5, tl, tr, br, bl))
+    img2 = up_sampling(get_image_old(5, 5, tl, tr, br, bl))
     cv2.imshow('image', img)
+    cv2.imshow('image2', img2)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+def get_image(width, height, color_tl, color_tr, color_br, color_bl):
+    """Creates game image (optimized)."""
+    colors = np.array([color_tl, color_tr, color_br, color_bl])
+    w_matrix = get_weight_matrix(width, height)
+    return np.uint8(np.matmul(w_matrix, colors))
+
+def get_weight_matrix(width, height):
+    """Calculates the weights' matrix."""
+    colvals = np.linspace(0, 1, width)
+    rowvals = np.linspace(0, 1, height)
+    xx, yy = np.meshgrid(rowvals, colvals)
+    # bottom right weight matrix
+    brw = xx * yy
+    #bottom left weight matrix
+    blw = np.fliplr(brw)
+    # top right weight matrix
+    trw = np.flipud(brw)
+    # top left weight matrix
+    tlw = np.fliplr(trw)
+    return np.dstack((tlw, trw, brw, blw))
 
 def up_sampling(image, new_width=640, new_height=640):
     """Up sampling image by repeat."""
@@ -20,11 +48,11 @@ def up_sampling(image, new_width=640, new_height=640):
     scale_y = int(new_height / height)
     return np.repeat(np.repeat(image, scale_x, axis=0), scale_y, axis=1)
 
-def get_image(width, height, color_tl, color_tr, color_br, color_bl):
-    """Create game image."""
+def get_image_old(width, height, color_tl, color_tr, color_br, color_bl):
+    """Creates game image."""
     l_col = get_colors(color_tl, color_bl, height)
     r_col = get_colors(color_tr, color_br, height)
-    rows = [get_colors(c1, c2, width) for (c1, c2) in zip(r_col, l_col)]
+    rows = [get_colors(c1, c2, width) for (c1, c2) in zip(l_col, r_col)]
     return np.array(rows)
 
 def get_colors(rgb1, rgb2, num):
@@ -37,10 +65,10 @@ def get_colors(rgb1, rgb2, num):
     return np.stack((blues, greens, reds), axis=1)
 
 def get_linspace(value1, value2, num):
-    """Calculate linspace given values."""
+    """Calculates linspace given values."""
     if value1 < value2:
         return np.linspace(value1, value2, num)
-    return np.linspace(value2, value1, num)
+    return np.linspace(value2, value1, num)[::-1]
 
 if __name__ == '__main__':
     main()
